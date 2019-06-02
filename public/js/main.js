829ad59fb079933067be94a0f020c4ab3907603b
Ruilin Zhang <ruilz@uci.edu>
2019-06-01 17:29:29 -0700

    updated main.js

--- public/js/main.js
+++ public/js/main.js
@@ -356,36 +356,28 @@ if(socket.id == payload.game.player_white.socket) {
       }
 
       if(old_board[row][column] != board[row][column]) {
-    			if(old_board[row][column] == '?' && board [row][column] == ' '){
-    			    $('#'+row+'_'+column).html('<img src="assets/imgs/empty.gif" alt="empty square"/>');
-    			}
-    		    else if(old_board[row][column] = '?' && board [row][column] == 'w'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/empty_white.gif" alt="white square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == 'b'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/empty_black.gif" alt="black square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == 'w'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/empty_white.gif" alt="white square"/>');
-                 }     
-                else if(old_board[row][column] = '?' && board [row][column] == 'b'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/empty_black.gif" alt="black square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == ' '){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/white_empty.gif" alt="empty square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == ' '){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/black_empty.gif" alt="empty square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == 'b'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/white_black.gif" alt="black square"/>');
-                 }
-                else if(old_board[row][column] = '?' && board [row][column] == 'w'){
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/black_white.gif" alt="white square"/>');
-                 }
-                else {
-                     $('#'+row+'_'+column).html('<img src="assets/imgs/error.gif" alt="error square"/>');                
-                 }
+        if(old_board[row][column] == '?' && board[row][column] == ''){
+          $('#'+row+'_'+column).html('<img src="assets/imgs/empty.gif" alt="empty square">');
+        } else if (old_board[row][column] == '?' && board[row][column] == 'w') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/empty_white.gif" alt="white square">');
+        } else if (old_board[row][column] == '?' && board[row][column] == 'b') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/empty_black.gif" alt="black square">');
+        } else if (old_board[row][column] == '' && board[row][column] == 'w') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/empty_white.gif" alt="white square">');
+        } else if (old_board[row][column] == '' && board[row][column] == 'b') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/empty_black.gif" alt="black square">');
+        } else if (old_board[row][column] == 'w' && board[row][column] == '') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/white_empty.gif" alt="empty square">');
+        } else if (old_board[row][column] == 'b' && board[row][column] == '') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/black_empty.gif" alt="empty square">');
+        } else if (old_board[row][column] == 'w' && board[row][column] == 'b') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/white_black.gif" alt="black square">');
+        } else if (old_board[row][column] == 'b' && board[row][column] == 'w') {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/black_white.gif" alt="white square">');
+        } else {
+          $('#'+row+'_'+column).html('<img src="assets/imgs/error.gif" alt="error square">');
+        }
+
 
              }
 
